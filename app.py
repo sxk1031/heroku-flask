258fd55d1f6e25c88878d99a014466e4df12f5ca
saurabh <saurabh@Ts-MacBook-Pro.local>
2020-09-16 11:32:44 -0400

    interactiev sine deleted

--- app.py
+++ app.py
@@ -10,23 +10,5 @@ def index():
 def about():
   return render_template('about.html')
 
-import numpy as np
-import matplotlib.pyplot as plt
-from ipywidgets import interactive
-
-def plot_func(a, f):
-    plt.figure(2)
-    x = np.linspace(0, 2*np.pi, num=1000)
-    y = a*np.sin(1/f*x)
-    plt.plot(x,y)
-    plt.ylim(-1.1, 1.1)
-    plt.title('a sin(f)')
-    plt.show()
-
-interactive_plot = interactive(plot_func, a=(-1,0,0.1), f=(0.1, 1))
-output = interactive_plot.children[-1]
-output.layout.height = '300px'
-interactive_plot
-
 if __name__ == '__main__':
   app.run(port=33507)
