1af7fc055d874b3f46749724a0ff20ed782d8978
saurabh <saurabh@Ts-MacBook-Pro.local>
2020-09-16 16:38:46 -0400

    author+journal list added

--- app.py
+++ app.py
@@ -16,11 +16,12 @@ from collections import Counter
 from numpy import percentile
 
 app = Flask(__name__)
+#from models import Result
 
 @app.route('/',methods=['GET','POST'])
 def index():
   errors = []
-  results = {}
+  results = []
   if request.method == 'POST':
     try:
        unique_authors = []
@@ -30,9 +31,35 @@ def index():
 #       start_vals = np.arange(0,np.divmod(total_papers,start)[0])*start
        vals = 0
        url = f'https://arxiv.org/search/advanced?advanced=&terms-0-operator=AND&terms-0-term=&terms-0-field=all&classification-physics=y&classification-physics_archives=all&classification-include_cross_list=include&date-year=&date-filter_by=date_range&date-from_date=1993-01-01&date-to_date=&date-date_type=announced_date_first&abstracts=hide&size=200&order=announced_date_first&start={vals}'
+       html = request.urlopen(url).read()
+       soup = BeautifulSoup(html)
+       for script in soup(["script", "style"]):
+           script.decompose()
+       strips = list(soup.stripped_strings)
+
+       auth_sw = 0    
+       journal_sw = 0
+       for string in strips:
+           if (journal_sw == 1):           
+               re_journ = re.search(r'[a-zA-Z\&.\s]+',string)
+               if re_journ:
+                   re_journ_clean = re.sub(' ','',re_journ.group())
+                   re_journ_clean = re.sub(r'\.','',re_journ_clean)
+                   journal_list += [re_journ_clean.lower()]  
+                   journal_sw = 0
+           if (auth_sw == 1):
+               if (string != ',' and string != 'Submitted'):
+                   unique_authors += [string]  
+           if (string == 'Authors:'):
+               auth_sw = 1    
+           if (string == 'Submitted'):
+               auth_sw = 0
+           if (string == 'Journal ref:'):    
+               journal_sw = 1
+    	return render_template('index.html', results=results)  
     except:
          errors.append('url not found')     
-  return render_template('index.html', errors=errors)
+  	 return render_template('index.html', errors=errors)
 
 @app.route('/about')
 def about():
