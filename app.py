d40f683e51398a2621c3bece97922582c86c0bbe
saurabh <saurabh@Ts-MacBook-Pro.local>
2020-09-16 15:29:46 -0400

    import libs added

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
 from flask import Flask, render_template, request, redirect
+import numpy as np
+import pandas as pd
+import re
+import urllib.request as request
+from bs4 import BeautifulSoup
+from collections import Counter
+import time
+import matplotlib.pyplot as plt
+%matplotlib inline
+import time
+from matplotlib.backends.backend_pdf import PdfPages
+import urllib.parse
+import unidecode
+import seaborn as sns
+from collections import Counter
+from numpy import percentile
 
 app = Flask(__name__)
 
