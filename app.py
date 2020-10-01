b405d519d3ffe31350763b1ba3fa0cbea01ad06d
saurabh <saurabh@ts-macbook-pro.attlocal.net>
2020-09-30 20:35:42 -0400

    dill open added

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ import seaborn as sns
 from collections import Counter
 from numpy import percentile
 from journal_name_dictionary import journal_name_dict
+import dill
 
 app = Flask(__name__)
 #from models import Result
@@ -24,39 +25,8 @@ def index():
 	errors = []
 	results = {}
 	if request.method == 'POST':
-		unique_authors = []
-		journal_list = []
-		start = 200
-		total_papers = request.form['number']
-		start_vals = np.arange(0,np.divmod(int(total_papers),start)[0])*start
-
-		for vals in start_vals:
-			url = f'https://arxiv.org/search/advanced?advanced=&terms-0-operator=AND&terms-0-term=&terms-0-field=all&classification-physics=y&classification-physics_archives=all&classification-include_cross_list=include&date-year=&date-filter_by=date_range&date-from_date=1993-01-01&date-to_date=&date-date_type=announced_date_first&abstracts=hide&size=200&order=announced_date_first&start={vals}'
-			html = urequest.urlopen(url).read()
-			soup = BeautifulSoup(html,features="html.parser")
-			for script in soup(["script", "style"]):
-				script.decompose()
-			strips = list(soup.stripped_strings)
-
-			auth_sw = 0    
-			journal_sw = 0
-			for string in strips:
-				if (journal_sw == 1):           
-					re_journ = re.search(r'[a-zA-Z\&.\s]+',string)
-					if re_journ:
-						re_journ_clean = re.sub(' ','',re_journ.group())
-						re_journ_clean = re.sub(r'\.','',re_journ_clean)
-						journal_list += [re_journ_clean.lower()]  
-						journal_sw = 0
-				if (auth_sw == 1):
-					if (string != ',' and string != 'Submitted'):
-						unique_authors += [string]  
-				if (string == 'Authors:'):
-					auth_sw = 1    
-				if (string == 'Submitted'):
-					auth_sw = 0
-				if (string == 'Journal ref:'):    
-					journal_sw = 1
+		unique_authors = dill.load(open('unique_authors.pkd', 'rb'))
+		journal_list = dill.load(open('journal_list.pkd', 'rb'))
 #		results=unique_authors
 		top_journals = Counter(journal_list).most_common(25)
 		results = top_journals
