035344f7ddfd78d2ae0364a4485d51123e389c39
saurabh <saurabh@ts-macbook-pro.attlocal.net>
2020-09-30 21:00:06 -0400

    .pkd copied

--- app.py
+++ app.py
@@ -25,9 +25,8 @@ def index():
 	errors = []
 	results = {}
 	if request.method == 'POST':
-		directory = '/Users/saurabh/Google\ Drive/Data_Science_Machine_Learning/astro-ph/'
-		unique_authors = dill.load(open(directory+'unique_authors.pkd', 'rb'))
-		journal_list = dill.load(open(directory+'journal_list.pkd', 'rb'))
+		unique_authors = dill.load(open('unique_authors.pkd', 'rb'))
+		journal_list = dill.load(open('journal_list.pkd', 'rb'))
 #		results=unique_authors
 		top_journals = Counter(journal_list).most_common(25)
 		results = top_journals
